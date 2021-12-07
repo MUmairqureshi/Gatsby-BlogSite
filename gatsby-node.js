ce44a126b0a5acd5c6a7afab161c94c530a20123
MUmairqureshi <67610498+MUmairqureshi@users.noreply.github.com>
2021-12-07 08:36:47 -0800

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,30 @@
-exports.createPages = async ({ actions }) => {
+const { ENETUNREACH } = require('constants')
+var path = require('path')
+
+exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+
+  const result = await graphql(`
+   {
+    allContentfulBlogSite {
+      edges {
+        node {
+          slug
+        }
+      }
+    }
+  }  
+     `)
+  console.log(JSON.stringify(result))
+
+  result.data.allContentfulBlogSite.edges.forEach((result) => {
+    console.log("data ", result)
+    createPage({
+      path: `/blog/${result.node.slug}`,
+      component: path.resolve('./src/templates/blog.tsx'),
+      context: {
+        slug: result.node.slug
+      }
+    })
   })
 }
