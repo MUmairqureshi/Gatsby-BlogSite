db74e33482036d8ba6cb6e45a5d81cefacfbd8c7
MUmairqureshi <67610498+MUmairqureshi@users.noreply.github.com>
2021-12-07 08:29:17 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,91 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import './bloglist.css'
+import { graphql, Link, useStaticQuery } from "gatsby";
+import { Child } from "../components/childern"
+import Img from 'gatsby-image';
 import { StaticImage } from "gatsby-plugin-image"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+// interface  Props{
+//   //   author : String ,
+//   //   title : String ,
+//   //   blogTitle : String ,
+//   //   blogImage :
+  
+//   // }
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
 
-export default IndexPage
+export default function Blog() {
+
+  const query = useStaticQuery(
+    graphql`
+ {
+      allContentfulBlogSite {
+        edges {
+          node {
+            author
+            slug
+            title
+            blogTitle
+            blogImage {
+                fluid {
+
+                    ...GatsbyContentfulFluid
+                  }
+                      }
+
+ 
+        
+            richDesc {
+              raw
+            }
+            blogSubtitle {
+              blogSubtitle
+            }
+            data(formatString: "MMM D YYYY")
+            subDesc {
+              subDesc
+            }
+          }
+        }
+      }
+    }
+          
+`)
+
+  return (
+    <div>
+
+      <Child>
+        <h1 className="title" >  My ALL CMS BLOG </h1>
+
+        <p className="text" >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the <br />
+          industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type<br />
+          and scrambled it to make a type specimen book. It has survived not only five centuries, but also<br />
+          the leap into electronic typesetting, remaining essentially unchanged. It was popularised<br />
+          in the 1960s </p>
+
+        <ul>
+          {query.allContentfulBlogSite.edges.map((ind) => {
+
+            return (
+
+              <li className="post" >
+                <span className="postImages"> <Img fluid={ind.node.blogImage[0]?.fluid} /> </span>
+                <div className="text-container" >
+                  <span  >    <Link to={`/blog/${ind.node.slug}`} >  <h2 className="posttitle" >  {ind.node.blogTitle}</h2> </Link>  </span  >
+                  <span className="posttext" > {documentToReactComponents(JSON.parse(ind.node.richDesc.raw))}</span>
+                </div>
+
+              </li>
+
+
+            )
+          })
+          }
+        </ul>
+
+      </Child>
+    </div>
+  )
+}
